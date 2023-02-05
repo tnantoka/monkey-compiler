f47ea4e982be44d620e6a3265458d4f1bd9e9861
tnantoka <tnantoka@bornneet.com>
2023-02-05 10:08:14 +0900

    feat: pop

--- vm/vm.go
+++ vm/vm.go
@@ -55,6 +55,9 @@ func (vm *VM) Run() error {
 			result := leftValue + rightValue
 			vm.push(&object.Integer{Value: result})
 
+		case code.OpPop:
+			vm.pop()
+
 		}
 	}
 
@@ -76,4 +79,8 @@ func (vm *VM) pop() object.Object {
 	o := vm.stack[vm.sp-1]
 	vm.sp--
 	return o
+}
+
+func (vm *VM) LastPoppedStackElem() object.Object {
+	return vm.stack[vm.sp]
 }
